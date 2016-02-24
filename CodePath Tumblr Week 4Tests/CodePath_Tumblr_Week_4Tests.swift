c6fceaa68303ee80808e726d29fce0575e6f4c49
Jess <jesswlam@gmail.com>
2016-02-24 08:52:41 -0800

    Initial Commit

---
+++ CodePath Tumblr Week 4Tests/CodePath_Tumblr_Week_4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CodePath_Tumblr_Week_4Tests.swift
+//  CodePath Tumblr Week 4Tests
+//
+//  Created by Jess Lam on 2/24/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import XCTest
+@testable import CodePath_Tumblr_Week_4
+
+class CodePath_Tumblr_Week_4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

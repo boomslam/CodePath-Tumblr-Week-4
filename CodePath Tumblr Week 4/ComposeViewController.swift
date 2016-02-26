fb3e51dbcdc323bd36c3699e158b8790e73b1c63
Jess <jesswlam@gmail.com>
2016-02-25 23:35:18 -0800

    Created custom tab bar view controller, view controllers, animated compose view buttons, and added scroll view for trending view.

--- CodePath Tumblr Week 4/ComposeViewController.swift
+++ CodePath Tumblr Week 4/ComposeViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet weak var textButton: UIButton!
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var quoteButton: UIButton!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var chatButton: UIButton!
+    @IBOutlet weak var linkButton: UIButton!
+    @IBOutlet weak var nevermindButton: UIButton!
+
+    var photoStartPosition: CGFloat!
+    var photoOffset: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Start position for photo button
+        photoStartPosition = photoButton.frame.origin.y
+        photoOffset = 600
 
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +36,179 @@ class ComposeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.photoButton.center.y = 580
+        self.quoteButton.center.y = 580
+        self.textButton.center.y = 580
+        self.chatButton.center.y = 580
+        self.linkButton.center.y = 580
+        self.videoButton.center.y = 580
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //set initial offscreen position and animate buttons onto screen
+//        self.photoButton.center.y = 600
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            self.photoButton.center.y = 198
+            }) { (Bool) -> Void in
+        }
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.quoteButton.center.y = 198
+            }) { (Bool) -> Void in
+        }
+        
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            self.textButton.center.y = 198
+            }) { (Bool) -> Void in
+        }
+        
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.chatButton.center.y = 320
+            }) { (Bool) -> Void in
+        }
+        
+        UIView.animateWithDuration(0.6, animations: { () -> Void in
+            self.linkButton.center.y = 320
+            }) { (Bool) -> Void in
+        }
+        
+        UIView.animateWithDuration(0.7, animations: { () -> Void in
+            self.videoButton.center.y = 320
+            }) { (Bool) -> Void in
+        }
+    }
+    
+    @IBAction func onTapDismiss(sender: AnyObject) {
+        print("did tap")
+
+        self.photoButton.center.y = 198
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            self.photoButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        self.quoteButton.center.y = 198
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.quoteButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        self.textButton.center.y = 198
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            self.textButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        self.chatButton.center.y = 320
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.chatButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        self.linkButton.center.y = 320
+        UIView.animateWithDuration(0.6, animations: { () -> Void in
+            self.linkButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        self.videoButton.center.y = 320
+        UIView.animateWithDuration(0.7, animations: { () -> Void in
+            self.videoButton.center.y = -300
+            }) { (Bool) -> Void in
+        }
+        
+        let seconds = 0.8
+        let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
+        dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+            
+            self.photoButton.center.y = -900
+            self.quoteButton.center.y = -900
+            self.textButton.center.y = -900
+            self.chatButton.center.y = -900
+            self.linkButton.center.y = -900
+            self.videoButton.center.y = -900
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        })
+        
+        print(photoButton.center.y)
+    }
+    
+    
+//    @IBAction func didTapNevermind(sender: UITapGestureRecognizer) {
+//         
+//        if sender.state == UIGestureRecognizerState.Began {
+//            
+//        } else if sender.state == UIGestureRecognizerState.Changed {
+//            
+//            self.photoButton.center.y = 198
+//            UIView.animateWithDuration(0.2, animations: { () -> Void in
+//                self.photoButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//            
+//            self.quoteButton.center.y = 198
+//            UIView.animateWithDuration(0.3, animations: { () -> Void in
+//                self.quoteButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//            
+//            self.textButton.center.y = 198
+//            UIView.animateWithDuration(0.4, animations: { () -> Void in
+//                self.textButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//            
+//            self.chatButton.center.y = 320
+//            UIView.animateWithDuration(0.5, animations: { () -> Void in
+//                self.chatButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//            
+//            self.linkButton.center.y = 320
+//            UIView.animateWithDuration(0.6, animations: { () -> Void in
+//                self.linkButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//            
+//            self.videoButton.center.y = 320
+//            UIView.animateWithDuration(0.7, animations: { () -> Void in
+//                self.videoButton.center.y = -300
+//                }) { (Bool) -> Void in
+//            }
+//
+//          
+//            
+//        } else if sender.state == UIGestureRecognizerState.Ended {
+//            
+//            self.photoButton.center.y = -300
+//            self.quoteButton.center.y = -300
+//            self.textButton.center.y = -300
+//            self.chatButton.center.y = -300
+//            self.linkButton.center.y = -300
+//            self.videoButton.center.y = -300
+//            
+//            let seconds = 0.8
+//            let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
+//            let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+//            
+//            dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+//                
+//                self.dismissViewControllerAnimated(true, completion: nil)
+//                
+//            })
+//            
+//        }
+//
+//        
+//    }
+    
     
 
     /*
--- CodePath Tumblr Week 4/ViewController.swift
+++ CodePath Tumblr Week 4/ViewController.swift
@@ -12,15 +12,83 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
     
+    //outlet for array of buttons
+    @IBOutlet var buttons: [UIButton]!
+    
+    //variable for arrary of view controllers
+    var viewControllers: [UIViewController]!
+    
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+//    var composeViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    
+    //keeps track of button that was selected. Will link buttons tag value to variable.
+    var selectedIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Add view controllers to tab bar view controller
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+//        composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController")
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        
+        //Add view controllers to viewControllers array. Removed compose view controller.
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        
+        buttons[selectedIndex].selected = true
+        didPressTab(buttons[selectedIndex])
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //All tab buttons share this action
+    @IBAction func didPressTab(sender: UIButton) {
+        
+        //Keep track of previous button
+        let previousIndex = selectedIndex
+        
+        //Sets tag value for selected button
+        selectedIndex = sender.tag
+        
+        //Set previous button to non-selected
+        buttons[previousIndex].selected = false
+        print("unselected button")
+        
+        //Access previous view controller from array
+        let previousVC = viewControllers[previousIndex]
+        
+        //Remove previous view controller
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        //Set current selected button to selected
+        sender.selected = true
+        
+        //Access current view controller from array
+        let vc = viewControllers[selectedIndex]
+        
+        //Add new view controllers
+        addChildViewController(vc)
+        
+        //Adjust size of vc to match contentView and add it as subview to contentView
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        
+        //Call viewDidAppear method of vc being added
+        vc.didMoveToParentViewController(self)
+        
     }
 
 
